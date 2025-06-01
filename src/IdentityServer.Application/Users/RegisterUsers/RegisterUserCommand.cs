a5fd39aec40fc9f284b59cb380e327842d7463c1
Dogukan Muslukcu <Dogukanmuslukcu@gmail.com>
2025-06-01 14:13:08 +0300

    Feature :  Added user command handler

---
+++ src/IdentityServer.Application/Users/RegisterUsers/RegisterUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+public class RegisterUserCommand : IRequest<RegisterUserResponse>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ src/IdentityServer.Application/Users/RegisterUsers/RegisterUserResponse.cs
@@ -0,0 +1,8 @@
+﻿public class RegisterUserResponse
+{
+    public string Email { get; set; }
+    public string AccessToken { get; set; }
+    public DateTime AccessTokenExpiration { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiration { get; set; }
+}
